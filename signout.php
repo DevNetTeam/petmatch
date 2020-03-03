ffb158b727c81481ae1719b3b19bf5346f61e2d5
antutantu <59747976+antutantu@users.noreply.github.com>
2020-03-03 18:51:44 +0200

    fix

--- signout.php
+++ signout.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
@@ -7,6 +8,8 @@ require_once "classes/User.php";
 if (isset($_SESSION['user'])){
     session_destroy();
     header("Location: main.php");
+    ob_end_flush();
     die();
 }
 //echo "signed out";
+?>
