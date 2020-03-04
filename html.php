b21b53f4120e780ac849e6cfabd33dc608259374
antutantu <59747976+antutantu@users.noreply.github.com>
2020-03-04 12:16:46 +0200

    up

---
+++ html.php
@@ -0,0 +1,15 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>PetMatchmaking</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    </head>
+  <body>
+  </body>
+</html>
